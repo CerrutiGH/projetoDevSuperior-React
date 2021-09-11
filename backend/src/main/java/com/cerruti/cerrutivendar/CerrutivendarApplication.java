d20459ea0ceeae36e85fb8ef9f43af9a5ebc8b1f
CerrutiGH <bielhcsousa@gmail.com>
2021-09-11 15:10:17 -0300

    Projeto criado

---
+++ backend/src/main/java/com/cerruti/cerrutivendar/CerrutivendarApplication.java
@@ -0,0 +1,13 @@
+package com.cerruti.cerrutivendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CerrutivendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CerrutivendarApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/cerruti/cerrutivendar/CerrutivendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cerruti.cerrutivendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CerrutivendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
